9d29abda78be7f60c14ebb5dca6feb52351abb5c
Junyong Jung <thejunyongjung@gmail.com>
2026-04-08 13:49:32 -0700

    Saving PayrollCalculator

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class PayrollCalculator {
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("This is the Payroll Calculator");
+
+        System.out.print("Enter the number of employee: ");
+        String name = read.nextLine();
+        System.out.print("Enter the number of hours worked: ");
+        double hoursWorked = read.nextDouble();
+        System.out.print("Enter the pay rate: ");
+        double payRate = read.nextDouble();
+
+        double grossPay = hoursWorked * payRate;
+
+        System.out.printf("Employee: %s, Gross Pay: $%,.2f", name, grossPay);
+    }
 }
